0f7e5f8ce8d589e4fa02b6a31e79e7d0971acc5b
veronika <veronika.blazic@gmail.com>
2015-03-31 19:59:41 +0200

    bbabbaba

--- app/Models/ZakIp.php
+++ app/Models/ZakIp.php
@@ -1,10 +1,8 @@
-<?php namespace App;
+<?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ZakIp extends Model {
-    use SoftDeletes;
     protected $table = 'zaklenjeni_ip';
     protected $fillable = ['ip', 'datum_odklenitve'];
     protected $guarded = ['id'];
