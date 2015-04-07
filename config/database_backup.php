36f75a50f4b408ff0585574e1e720265a43fe6b2
Nejc Bizjak <neco.bizjak@gmail.com>
2015-04-07 16:07:25 +0200

    Popravljam server.

--- config/database_backup.php
+++ config/database_backup.php
@@ -170,15 +170,16 @@ return [
         ],
 
         'mysql' => [
-            'host'      => 'localhost',
             'driver'    => 'mysql',
-            'database' => 'studijska_informatika',
-            'port'      => '3306',
-            'username'  => 'root',
-            'password'  => '',
+            'host'      => env('DB_HOST', env('OPENSHIFT_MYSQL_DB_HOST', 'localhost')),
+            'port'      => env('DB_PORT', env('OPENSHIFT_MYSQL_DB_PORT', 3306)),
+            'database'  => env('DB_DATABASE', env('OPENSHIFT_APP_NAME', 'forge')),
+            'username'  => env('DB_USERNAME', env('OPENSHIFT_MYSQL_DB_USERNAME', 'forge')),
+            'password'  => env('DB_PASSWORD', env('OPENSHIFT_MYSQL_DB_PASSWORD', '')),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
+            'strict'    => false,
         ],
 
         'pgsql' => [
